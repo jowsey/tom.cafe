cc92653274a3185278f359f6ebdee9a5c98c78b7
Thomas Jowsey <2302820@uad.ac.uk>
2025-09-01 14:32:16 +0100

    Add project link & meta tags

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,24 +1,35 @@
 ﻿<script lang="ts">
-	import type { Snippet } from 'svelte';
+	import type { HTMLAnchorAttributes } from 'svelte/elements';
 
-	interface Props {
-		className?: string;
-		children?: Snippet;
-		onclick?: () => void;
+	interface Props extends HTMLAnchorAttributes {
 		enabled?: boolean;
+		icon?: string;
+		title?: string;
+		content?: string;
 	}
 
-	let { className, children, onclick, enabled = true }: Props = $props();
+	let { enabled = true, icon, title, content, ...attribs }: Props = $props();
 </script>
 
-<button
+<a
+	target="_blank"
+	{...attribs}
 	class={[
 		'rounded-md border-2 border-neutral-300/5 bg-neutral-50/10 px-4 py-2 shadow-neutral-200/20 backdrop-blur-md transition-all duration-75',
 		enabled && 'cursor-pointer hover:bg-neutral-50/20 hover:shadow',
-		className
+		attribs.class
 	]}
-	{onclick}
-	disabled={!enabled}
 >
-	{@render children?.()}
-</button>
+	<div class="flex items-center gap-x-3">
+		{#if icon}
+			<img src={icon} alt="Icon" class="size-5" />
+		{/if}
+		{#if title}
+			<p class="font-bold">{title}</p>
+		{/if}
+	</div>
+
+	{#if content}
+		<p class="mt-2 text-sm text-neutral-300">{content}</p>
+	{/if}
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
-<!--<script>-->
-<!--	import Button from '$lib/components/Button.svelte';-->
-<!--</script>-->
+<script>
+	import Button from '$lib/components/Button.svelte';
+</script>
 
-<!--<div class="text-sm text-neutral-300 grid grid-cols-1 gap-2">-->
-<!--	<Button enabled={false}>?</Button>-->
-<!--</div>-->
+<div class="grid grid-cols-1 gap-2">
+	<Button
+		icon="https://ow2.tom.cafe/Overwatch2_Primary_1C_WHITE.png"
+		title="Overwatch 2 Historical Data"
+		content="Tracking pick/win-rates for every Overwatch 2 hero over time."
+		href="https://ow2.tom.cafe"
+	/>
+</div>
